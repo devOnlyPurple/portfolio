3cce61317607680a4de32c530984708474d9903c
ONLY_PURPLE\dav08 <davio.sowa9@gmail.com>
2025-08-03 14:56:31 +0000

    frontend add

--- index.js
+++ index.js
@@ -2,14 +2,20 @@ const express = require("express");
 const AppConstants = require("./core/constants/app_constants");
 const axios = require("axios");
 const cron = require("node-cron");
+const path = require("path");
 const port = process.env.PORT || AppConstants.PORT;
 const app = express();
 app.use(express.static("public"));
+app.use(express.static(path.join(__dirname, "src")));
 app.use(express.json());
 const file = require("./controllers/cloud/file_controllers");
 app.get("/ping", (req, res) => {
   console.log("PING__PING");
   res.status(200).json({ message: "pong" });
+});
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "src", "index.html"));
 });
 
 cron.schedule("*/3 * * * *", async () => {
